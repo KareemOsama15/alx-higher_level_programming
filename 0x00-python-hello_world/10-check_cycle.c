ad7bc091ee593415dc89682977e40abacaca5ae5
KareemOsama15 <karim.osama.saleh@gmail.com>
2023-10-03 00:23:26 +0300

    [push] tasks of 0x00-python-hello_world from 7 to 10

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,16 +1,22 @@
 #include "lists.h"
 
+/**
+ * check_cycle - function checks if a singly linked list
+ *    has  a cycle in it
+ *
+ * @list: head of Linked List
+ * Return: 0 if no cycle , 1 if there a cycle
+ */
 int check_cycle(listint_t *list)
 {
-    listint_t *ptr1 = list, *ptr2 = list;
+	listint_t *ptr1 = list, *ptr2 = list;
 
-    while (ptr1 && ptr2 && ptr2->next)
-    {
-        // 0 1 2 3 98 1024
-        ptr1 = ptr1->next;
-        ptr2 = ptr2->next->next;
-        if (ptr1 == ptr2)
-            return (1);
-    }
-    return (0);
+	while (ptr1 && ptr2 && ptr2->next)
+	{
+		ptr1 = ptr1->next;
+		ptr2 = ptr2->next->next;
+		if (ptr1 == ptr2)
+			return (1);
+	}
+	return (0);
 }
