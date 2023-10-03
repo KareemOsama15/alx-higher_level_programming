8b2f441aee4b32904a5d0d168bbb2ed8204a7933
KareemOsama15 <karim.osama.saleh@gmail.com>
2023-10-03 09:20:39 +0300

    [push] 0x01-python-if_else_loops_functions project task 13

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+
+/**
+ * insert_node - function checks if a singly linked list
+ *    has  a cycle in it
+ *
+ * @head: head of Linked List
+ * @number: number
+ * Return: address of new node, or NULL if failed
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *newElement = NULL, *current = *head;
+
+	newElement = (struct listint_s *)malloc(sizeof(struct listint_s));
+	if (!newElement)
+		return (NULL);
+
+	newElement->n = number;
+	if (*head == NULL || current->data >= newElement->data)
+	{
+		newElement->next = *head;
+		*head = newElement;
+		return (newElement);
+	}
+	while (current)
+	{
+		/* 1 2 3 4 -27- 98 402 1024 */
+		if (current->next != NULL && current->next->n >= newElement->n)
+		{
+			newElement->next = current->next;
+			current->next = newElement;
+			return (newElement);
+		}
+		else if (current->next == NULL)
+		{
+			newElement->next = NULL;
+			current->next = newElement;
+			return (newElement);
+		}
+		current = current->next;
+	}
+	return (NULL);
+}
