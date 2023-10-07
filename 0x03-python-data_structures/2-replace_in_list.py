0296d13cc07afee54620d481dafebeb2e980f369
KareemOsama15 <karim.osama.saleh@gmail.com>
2023-10-07 19:58:49 +0300

    [push] 0x03-python-data_structures project tasks[0:6]

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,9 +2,7 @@
 
 def replace_in_list(my_list, idx, element):
     if idx < len(my_list) and idx >= 0:
-        for i in my_list:
-            if i == idx:
-                my_list[i] = element
+        my_list[idx] = element
         return (my_list)
     else:
         return (my_list)
