07268c2b6f88d9a3dbfe2286b44e2e3a5a3bdd41
KareemOsama15 <karim.osama.saleh@gmail.com>
2023-10-07 21:05:05 +0300

    [push] 0x03-python-data_structures project tasks[6, 9, 11]

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,6 +2,8 @@
 
 def print_matrix_integer(matrix=[[]]):
     for list in matrix:
+        i = 0
         for item in list:
-            print("{:d}".format(item), end=' ')
+            print("{:d}".format(item), end=(" " if i < len(list) - 1 else ""))
+            i += 1
         print()
---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if not my_list:
+        return (None)
+    else:
+        maxNum = my_list[0]
+        for i in range(len(my_list)):
+            if maxNum < my_list[i]:
+                maxNum  = my_list[i]
+        return (maxNum)
