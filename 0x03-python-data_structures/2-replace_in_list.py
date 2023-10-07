0eee7892088d33396c3686ffc4058c9c345615ca
KareemOsama15 <karim.osama.saleh@gmail.com>
2023-10-07 19:54:32 +0300

    [push] 0x03-python-data_structures project tasks[0:6]

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -5,7 +5,6 @@ def replace_in_list(my_list, idx, element):
         for i in my_list:
             if i == idx:
                 my_list[i] = element
-            break
         return (my_list)
     else:
         return (my_list)
