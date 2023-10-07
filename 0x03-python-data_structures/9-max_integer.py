78f12035e3abeced18d1e9910234bdbad758f968
KareemOsama15 <karim.osama.saleh@gmail.com>
2023-10-07 21:07:42 +0300

    [push] 0x03-python-data_structures project tasks[6, 9, 11]

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -6,5 +6,5 @@ def max_integer(my_list=[]):
         maxNum = my_list[0]
         for i in range(len(my_list)):
             if maxNum < my_list[i]:
-                maxNum  = my_list[i]
+                maxNum = my_list[i]
         return (maxNum)
