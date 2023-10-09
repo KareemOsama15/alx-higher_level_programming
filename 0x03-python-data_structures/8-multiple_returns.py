27b904c69559622e9c53003417cb43a998dd7c4c
KareemOsama15 <karim.osama.saleh@gmail.com>
2023-10-09 03:23:24 +0300

    [push] 0x03-python-data_structures project task[7, 8]

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def multiple_returns(sentence):
+    
+    length = len(sentence)
+    char = (sentence[0] if sentence else None)
+    newTuple = (length, char)
+
+    return (newTuple)
