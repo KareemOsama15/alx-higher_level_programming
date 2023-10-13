ab22c500424378c56afd2e415c863ec88a08356e
KareemOsama15 <karim.osama.saleh@gmail.com>
2023-10-13 21:56:32 +0300

    [push] project 0x04-python-more_data_structures tasks[12]

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,9 @@
 
 def roman_to_int(roman_string):
 
-    if not roman_string or isinstance(roman_string, str):
+    if not roman_string:
+        return (0)
+    if not isinstance(roman_string, str):
         return (0)
 
     romVal = {
