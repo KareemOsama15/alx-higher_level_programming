b967fbd505ca198731bff57c61e02e85453de997
KareemOsama15 <karim.osama.saleh@gmail.com>
2023-10-13 21:51:21 +0300

    [push] project 0x04-python-more_data_structures tasks[12]

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+
+def roman_to_int(roman_string):
+
+    if not roman_string:
+        return (0)
+
+    romVal = {
+        'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000
+    }
+    num = 0
+    romStr = roman_string[:]
+
+    for i in range(len(romStr)):
+        if i < len(romStr) - 1 and romVal[romStr[i]] < romVal[romStr[i + 1]]:
+            num -= romVal[romStr[i]]
+        else:
+            num += romVal[romStr[i]]
+    return (num)
