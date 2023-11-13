4c055c7e3461233f72952233eb2c5502e6ed0421
KareemOsama15 <karim.osama.saleh@gmail.com>
2023-11-14 01:38:00 +0200

    [push] project 0x0C-python-almost_a_circle tasks[0:19]

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -20,7 +20,7 @@ class Base:
 
     def to_json_string(list_dictionaries):
         """method that returns the JSON string representation"""
-        if list_dictionaries is None or list_dictionaries == "[]":
+        if list_dictionaries is None or list_dictionaries == []:
             return ("[]")
         return (json.dumps(list_dictionaries))
 
