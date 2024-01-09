758c6a1cbb79c5ba9d0555b1ccb03411f5fcce22
KareemOsama15 <karim.osama.saleh@gmail.com>
2024-01-09 09:44:30 +0200

    [push] project 0x12-javascript-warm_up tasks[0:13]

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,7 +3,7 @@ if (process.argv.length <= 3) {
   console.log(0);
 } else {
   let first = parseInt(process.argv[2]);
-  let second = first;
+  let second = parseInt(process.argv[3]);
   let current = 0;
   for (let i = 3; i < process.argv.length; i++) {
     current = parseInt(process.argv[i]);
