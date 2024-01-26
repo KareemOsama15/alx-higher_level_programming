04fe39758cb278745ab597a86ccb1ddeb82b9bd7
KareemOsama15 <karim.osama.saleh@gmail.com>
2024-01-27 01:38:31 +0200

    [push] project tasks[0:3]

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -10,7 +10,5 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     with request.urlopen(url) as response:
-        for item in response.headers.items():
-            if item[0] == 'X-Request-Id':
-                x_Request_Id = item[1]
-        print(x_Request_Id)
+        data = response.info()
+        print(data.get("X-Request-Id"))
