48c3d053b830e0d35f4c83b375e9abb9159fd10e
KareemOsama15 <karim.osama.saleh@gmail.com>
2024-02-21 00:12:38 +0200

    [push] project tasks[0,1]

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,6 +3,5 @@
 const path = process.argv[2];
 const fs = require('fs');
 fs.readFile(path, 'utf8', (err, data) => {
-    if (err) { console.log(err); }
-    else { console.log(data); }
+  if (err) { console.log(err); } else { console.log(data); }
 });
--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-const fs = require('fs')
-path = process.argv[2];
-content = process.argv[3];
+const fs = require('fs');
+const path = process.argv[2];
+const content = process.argv[3];
 fs.writeFile(path, content, 'utf8', (err) => {
-    if (err) { console.log(err); }
+  if (err) { console.log(err); }
 });
