b43b9401d4038e51ebf3dd3d6351511d7fa2a8fd
KareemOsama15 <karim.osama.saleh@gmail.com>
2024-02-21 01:52:27 +0200

    [push] project tasks[2,3]

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+
+const request = require('request');
+const options = {
+  url: process.argv[2],
+  method: 'GET'
+};
+request(options, (err, res) => {
+  if (err) { console.log(err); } else { console.log('code: ' + res.statusCode); }
+});
--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+
+const request = require('request');
+const id = process.argv[2];
+const options = {
+  url: 'https://swapi-api.alx-tools.com/api/films/' + id,
+  method: 'GET'
+};
+request(options, (err, res, body) => {
+  if (err) { console.log(err); } else {
+    const json = JSON.parse(body);
+    console.log(json.title);
+  }
+});
