bc081d479c9cb0a362ee072a2ad13fa7129bbb45
KareemOsama15 <karim.osama.saleh@gmail.com>
2024-02-21 03:51:06 +0200

    [push] project tasks[4]

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -5,15 +5,15 @@ const url = process.argv[2];
 const id = 18;
 
 request(url, (err, res, body) => {
-  if (err) { console.log(err); }
-
-  const results = JSON.parse(body).results;
-  let count = 0;
-  for (let i = 0; i < results.length; i++) {
-    for (const film of results[i].characters) {
-      if (film.includes('https://swapi-api.alx-tools.com/api/people/' + id)) {
-        count++;
-        break;
+  if (err) { console.log(err); } else {
+    const results = JSON.parse(body).results;
+    let count = 0;
+    for (let i = 0; i < results.length; i++) {
+      for (const film of results[i].characters) {
+        if (film.includes('https://swapi-api.alx-tools.com/api/people/' + id)) {
+          count++;
+          break;
+        }
       }
     }
     console.log(count);
